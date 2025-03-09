d033c3e52c5a563807759cba84df3b8748f905de
unubatsuuri <unu.batsuuri@mail.utoronto.ca>
2025-03-08 22:46:35 -0500

    n

--- T6/index.js
+++ T6/index.js
@@ -124,13 +124,13 @@ app.get("/notes/:noteId", basicAuth, async (req, res) => {
     }
 
     const note = await prisma.note.findUnique({ where: { id: noteId } });
-    if (note.userId !== req.user.id) {
-        return res.status(403).json({ message: "Not permitted" });
-    }
+    
     if (!note) {
         return res.status(404).json({ message: "Not found" });
     }
-
+    if (note.userId !== req.user.id) {
+        return res.status(403).json({ message: "Not permitted" });
+    }
     res.status(200).json(note);
 });
 
@@ -152,11 +152,12 @@ app.patch("/notes/:noteId", basicAuth, async (req, res) => {
     const { title, description, completed, public: isPublic } = req.body;
     
     const note = await prisma.note.findUnique({ where: { id: noteId } });
-    if (note.userId !== req.user.id) {
-        return res.status(403).json({ message: "Not permitted" });
-    }
+    
     if (!note) {
         return res.status(404).json({ message: "Not found" });
+    }
+    if (note.userId !== req.user.id) {
+        return res.status(403).json({ message: "Not permitted" });
     }
 
 
