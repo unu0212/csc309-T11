a5cb04d97289c85adfe7895983d1b4b2e50020c4
unubatsuuri <unu.batsuuri@mail.utoronto.ca>
2025-03-08 22:34:26 -0500

    n

--- T6/index.js
+++ T6/index.js
@@ -124,14 +124,14 @@ app.get("/notes/:noteId", basicAuth, async (req, res) => {
     }
 
     const note = await prisma.note.findUnique({ where: { id: noteId } });
-
+    if (note.userId !== req.user.id) {
+        return res.status(403).json({ message: "Not permitted" });
+    }
     if (!note) {
         return res.status(404).json({ message: "Not found" });
     }
 
-    if (note.userId !== req.user.id) {
-        return res.status(403).json({ message: "Not permitted" });
-    }
+    
 
     res.status(200).json(note);
 });
@@ -141,9 +141,11 @@ app.patch("/notes/:noteId", basicAuth, async (req, res) => {
     if (!req.user) {
         return res.status(401).json({ message: "Not authenticated" });
     }
-
+    if (note.userId !== req.user.id) {
+        return res.status(403).json({ message: "Not permitted" });
+    }
     if (Object.keys(req.body).length === 0) {
-        return res.status(404).json({ message: "Invalid payload" });
+        return res.status(400).json({ message: "Invalid payload" });
     }
 
     const noteId = parseInt(req.params.noteId, 10);
@@ -159,9 +161,6 @@ app.patch("/notes/:noteId", basicAuth, async (req, res) => {
         return res.status(404).json({ message: "Not found" });
     }
 
-    if (note.userId !== req.user.id) {
-        return res.status(403).json({ message: "Not permitted" });
-    }
 
     const updatedNote = await prisma.note.update({
         where: { id: noteId },
