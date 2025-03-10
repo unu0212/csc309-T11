745744c8f303ef474c442eee7ed67963061ebe40
batsuuri <markus@markus.com>
2025-03-09 23:03:28 -0400

    Update files

---
+++ A2/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+'use strict';
+
+const port = (() => {
+    const args = process.argv;
+
+    if (args.length !== 3) {
+        console.error("usage: node index.js port");
+        process.exit(1);
+    }
+
+    const num = parseInt(args[2], 10);
+    if (isNaN(num)) {
+        console.error("error: argument must be an integer.");
+        process.exit(1);
+    }
+
+    return num;
+})();
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// ADD YOUR WORK HERE
+
+
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+server.on('error', (err) => {
+    console.error(`cannot start server: ${err.message}`);
+    process.exit(1);
+});
