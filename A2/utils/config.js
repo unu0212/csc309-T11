b4da5d7d6eca0f3b5513cea3508b4a5260537f72
unubatsuuri <unu.batsuuri@mail.utoronto.ca>
2025-03-21 03:19:53 -0400

    adding config

--- A2/utils/config.js
+++ A2/utils/config.js
@@ -1,5 +1,6 @@
-require("dotenv").config(); 
+
 
 module.exports = {
-    SECRET_KEY: process.env.SECRET_KEY || "default_fallback_key" 
-};
+    SECRET_KEY: "anehu9327gnv48nqpn475gt4nw8p43cno4dfh"
+  };
+  
