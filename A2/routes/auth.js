7ed594a97eeca23c17bec73ae5516ade5d79d01a
unubatsuuri <unu.batsuuri@mail.utoronto.ca>
2025-03-21 03:26:22 -0400

    auth.js required fields

--- A2/routes/auth.js
+++ A2/routes/auth.js
@@ -6,6 +6,9 @@ const router = express.Router();
 // Authenticate User & Generate Token
 router.post("/tokens", async (req, res) => {
     const { utorid, password } = req.body;
+    if(!utorid || !password){
+        return res.status(400).json({message: "invalid payload for login"});
+    }
     const result = await AuthService.authenticateUser(utorid, password);
     return res.status(result.status).json(result.data || { message: result.message });
 });
@@ -13,6 +16,9 @@ router.post("/tokens", async (req, res) => {
 // Request Password Reset
 router.post("/resets", rateLimit, async (req, res) => {
     const { utorid } = req.body;
+    if(!utorid){
+        return res.status(400).json({message: "invalid payload for reset"});
+    }
     const result = await AuthService.requestPasswordReset(utorid);
     return res.status(result.status).json(result.data || { message: result.message });
 });
@@ -20,7 +26,13 @@ router.post("/resets", rateLimit, async (req, res) => {
 // Reset Password
 router.post("/resets/:resetToken", async (req, res) => {
     const { utorid, password } = req.body;
+    if(!utorid || !password){
+        return res.status(400).json({message: "invalid payload for resettoken"});
+    }
     const { resetToken } = req.params;
+    if(!resetToken){
+        return res.status(400).json({message: "needreset token"});
+    }
     const result = await AuthService.resetPassword(resetToken, utorid, password);
     return res.status(result.status).json(result.data || { message: result.message });
 });
