c8f355a0a2a40b67e576a5bf69455fd93a005ef6
unu0212 <144357704+unu0212@users.noreply.github.com>
2025-04-06 00:33:35 -0400

    Delete A2 directory

--- A2/utils/config.js
+++
@@ -1,6 +0,0 @@
-
-
-module.exports = {
-    SECRET_KEY: "anehu9327gnv48nqpn475gt4nw8p43cno4dfh"
-  };
-  
--- A2/utils/tokenHelpers.js
+++
@@ -1,8 +0,0 @@
-const { v4: uuidv4 } = require("uuid");
-
-// Generate a One-Time Reset Token (UUID)
-function generateResetToken() {
-    return uuidv4();
-}
-
-module.exports = { generateResetToken };
